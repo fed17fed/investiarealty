6f4b715913affd7afe76c620fe5246a6096b7fe6
Alexey <fedorov.kav17@gmail.com>
2023-12-28 18:16:36 +0200

    form02

--- pages/myPage.js
+++ pages/myPage.js
@@ -1,85 +1,85 @@
-import React from 'react';
-const API_URL = process.env.NEXT_PUBLIC_MY_WORDPRESS_API_URL
+// import React from 'react';
+// const API_URL = process.env.NEXT_PUBLIC_MY_WORDPRESS_API_URL
 
-const MyPage = ( data ) => {
-    console.log('Myuser = ', data);
-  // Render your page with the fetched data
-  return <div>{data?.title}</div>;
-};
+// const MyPage = ( data ) => {
+//     console.log('Myuser = ', data);
+//   // Render your page with the fetched data
+//   return <div>{data?.title}</div>;
+// };
 
-export async function getServerSideProps(context, query) {
+// export async function getServerSideProps(context, query) {
 
-  const headers =  { 'Content-Type': 'application/json'};
-  const response = await fetch(API_URL, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify({ query:`
-    query Objects {
-      realtyObjects {
-        edges {
-          node {
-            Real_estate_object {
-              vendorCode
-              title
-              forrent
-              price
-              sqft
-              city
-              location
-              propertytype
-              yearbuilding
-              lat
-              long
-              bed
-              bath
-              featured
-              mainImage {
-                sourceUrl
-              }
-              gallery {
-                image {
-                  link
-                }
-              }
-              features {
-                airconditioning
-                dryer
-                fieldGroupName
-                frontyard
-                lakeview
-                lawn
-                outdoorshower
-                refrigerator
-                tvcable
-                washer
-                winecellar
-              }
-              catobject {
-                tags
-              }
-            }
-          }
-        }
-      }
-    }
-    `})
-  });
+//   const headers =  { 'Content-Type': 'application/json'};
+//   const response = await fetch(API_URL, {
+//     method: 'POST',
+//     headers,
+//     body: JSON.stringify({ query:`
+//     query Objects {
+//       realtyObjects {
+//         edges {
+//           node {
+//             Real_estate_object {
+//               vendorCode
+//               title
+//               forrent
+//               price
+//               sqft
+//               city
+//               location
+//               propertytype
+//               yearbuilding
+//               lat
+//               long
+//               bed
+//               bath
+//               featured
+//               mainImage {
+//                 sourceUrl
+//               }
+//               gallery {
+//                 image {
+//                   link
+//                 }
+//               }
+//               features {
+//                 airconditioning
+//                 dryer
+//                 fieldGroupName
+//                 frontyard
+//                 lakeview
+//                 lawn
+//                 outdoorshower
+//                 refrigerator
+//                 tvcable
+//                 washer
+//                 winecellar
+//               }
+//               catobject {
+//                 tags
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     `})
+//   });
 
-  if (!response.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!response.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
   
-  const data = await response.json(); 
+//   const data = await response.json(); 
 
 
-  const { data: {realtyObjects: { edges }}} = data
-  const lists = edges?.map((item) => item.node.Real_estate_object )
+//   const { data: {realtyObjects: { edges }}} = data
+//   const lists = edges?.map((item) => item.node.Real_estate_object )
   
 
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
-}
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   };
+// }
 
-export default MyPage;
+// export default MyPage;
--- pages/realty.js
+++ pages/realty.js
@@ -1,76 +1,76 @@
-const API_URL = process.env.NEXT_PUBLIC_MY_WORDPRESS_API_URL
-export default async function Realty(query, req, res) {
-  const headers =  { 'Content-Type': 'application/json'};
-  const response = await fetch(API_URL, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify({ query:`
-    query Objects {
-      realtyObjects {
-        edges {
-          node {
-            Real_estate_object {
-              vendorCode
-              title
-              forrent
-              price
-              sqft
-              city
-              location
-              propertytype
-              yearbuilding
-              lat
-              long
-              bed
-              bath
-              featured
-              mainImage {
-                sourceUrl
-              }
-              gallery {
-                image {
-                  link
-                }
-              }
-              features {
-                airconditioning
-                dryer
-                fieldGroupName
-                frontyard
-                lakeview
-                lawn
-                outdoorshower
-                refrigerator
-                tvcable
-                washer
-                winecellar
-              }
-              catobject {
-                tags
-              }
-            }
-          }
-        }
-      }
-    }
-    `})
-  });
+// const API_URL = process.env.NEXT_PUBLIC_MY_WORDPRESS_API_URL
+// export default async function Realty(query, req, res) {
+//   const headers =  { 'Content-Type': 'application/json'};
+//   const response = await fetch(API_URL, {
+//     method: 'POST',
+//     headers,
+//     body: JSON.stringify({ query:`
+//     query Objects {
+//       realtyObjects {
+//         edges {
+//           node {
+//             Real_estate_object {
+//               vendorCode
+//               title
+//               forrent
+//               price
+//               sqft
+//               city
+//               location
+//               propertytype
+//               yearbuilding
+//               lat
+//               long
+//               bed
+//               bath
+//               featured
+//               mainImage {
+//                 sourceUrl
+//               }
+//               gallery {
+//                 image {
+//                   link
+//                 }
+//               }
+//               features {
+//                 airconditioning
+//                 dryer
+//                 fieldGroupName
+//                 frontyard
+//                 lakeview
+//                 lawn
+//                 outdoorshower
+//                 refrigerator
+//                 tvcable
+//                 washer
+//                 winecellar
+//               }
+//               catobject {
+//                 tags
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     `})
+//   });
 
-  if (!response.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!response.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
   
-  const data = await response.json();  
+//   const data = await response.json();  
 
   
 
-  const { data: {realtyObjects: { edges }}} = data
-  const lists = edges?.map((item) => item.node.Real_estate_object )
-   console.log('Myuser = ', lists);
+//   const { data: {realtyObjects: { edges }}} = data
+//   const lists = edges?.map((item) => item.node.Real_estate_object )
+//    console.log('Myuser = ', lists);
 
-   return  lists   
+//    return  lists   
   
-}
+// }
 
-Realty()
+// Realty()
